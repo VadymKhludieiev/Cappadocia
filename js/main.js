ce98b417c8bf163f430eade1a6d4cbfffeb8564c
VadymKhludieiev <156354412+VadymKhludieiev@users.noreply.github.com>
2024-09-06 23:16:44 +0200

    end

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+$('.info__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+autoplay: true,
+fade: true
+});
+});
